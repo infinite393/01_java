62823cbbfb207152dcc03b1252d8a52c7ff71fef
infinite393 <skfo3219@gmail.com>
2024-05-16 09:37:57 +0900

    object

---
+++ chapter06-class-and-object-lecture/src/main/java/com/ohgiraffers/practice/question/Application01.java
@@ -0,0 +1,9 @@
+package main.java.com.ohgiraffers.practice.question;
+
+public class Application01 {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ chapter06-class-and-object-lecture/src/main/java/com/ohgiraffers/practice/question/Monster.java
@@ -0,0 +1,12 @@
+package main.java.com.ohgiraffers.practice.question;
+
+public class Monster {
+
+    private String name;
+    private int age;
+
+    public void monsterName (){
+
+    }
+
+}
