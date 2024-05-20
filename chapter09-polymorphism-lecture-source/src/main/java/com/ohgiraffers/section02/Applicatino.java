7ae5b2fe19be47e7fb16fc6fc0644fefc613ec0d
infinite393 <skfo3219@gmail.com>
2024-05-20 10:15:44 +0900

    polymorphism

---
+++ chapter09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section02/Applicatino.java
@@ -0,0 +1,10 @@
+package main.java.com.ohgiraffers.section02;
+
+public class Applicatino {
+    public static void main(String[] args) {
+
+        /*
+        * 추상클래스와 추상 메소드
+         */
+    }
+}
